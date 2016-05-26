dd961514c034187ce331482a07e3038e805755ec
Guillermo <gfrubi@gmail.com>
2016-05-25 20:46:43 -0400

    calculo mas compacto de scattering the Thomson

--- figuras-editables/fig-mas-potencia-total.py
+++ figuras-editables/fig-mas-potencia-total.py
@@ -1,31 +1,27 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri May 17 12:56:14 2013
 
-@author: gr
-"""
-
-import numpy
+from numpy import *
 from scipy import integrate
-from scipy.special import *
-from pylab import *
+from scipy.special import jv
+from matplotlib.pyplot import *
 
-b=0.5
-m=numpy.array(range(1,6))
-plt.figure()
+
 # se define el resultado de la integral, para cada m como una función
 def f(m):
         f=lambda theta: (b**2)*m**2*(tan(theta)**2)*jv(m,b*m*cos(theta))**2*sin(theta)
-# calcula la integral, usando el método de cuadraturas
         return integrate.quad(f, 0, pi)[0]
-# calcula la integral para toda la lista de valores de m
-y=numpy.array(map(f,m))
+fv=vectorize(f)
+
+b=0.5
+m=arange(1,6)
+y=fv(m)
 # normaliza la integral, dividiendo por la suma sobre todos los m's
-ynorm=y/numpy.sum(y)
+ynorm=y/sum(y)
 #grafica 
+figure()
 plot(m,ynorm,'ro')
 axis([0.8,5.2,0,1])
-grid(True)
+grid()
 minorticks_on()
 xlabel('$m$')
 ylabel('$\left\langle {P_m}\\right\\rangle $')
