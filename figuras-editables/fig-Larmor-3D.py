0b351e055d9719d4ef0138d5a3b38d5c8f45877f
Guillermo Rubilar <gfrubi@gmail.com>
2020-07-06 20:31:11 -0400

    plt explicito

--- figuras-editables/fig-Larmor-3D.py
+++ figuras-editables/fig-Larmor-3D.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
+plt.style.use('classic')
 from numpy import *
 
-fig = figure()
+fig = plt.figure()
 ax = Axes3D(fig)
 ax.set_axis_off() # desactiva ejes
 
@@ -11,7 +12,7 @@ t = linspace(0, pi, 100)
 p = linspace(0, 2*pi-pi/2, 100)
 theta,phi = meshgrid(t,p)
 
-r= (sin(theta))**2
+r = (sin(theta))**2
 x = r * sin(theta) * cos(phi)
 y = r * sin(theta) * sin(phi)
 z = r * cos(theta)
@@ -20,4 +21,4 @@ ax.plot_surface(x, y, z, rstride=4, cstride=4, color='red', alpha=0.5)
 #ax.view_init(elev=45, azim=0)
 ax.quiver(0,0,0,0,0,1, length=0.7, arrow_length_ratio=0.15, pivot='tail', color='blue', linewidth=5)
 ax.text(0.1,0,0.60, r'$\vec{a}$', fontsize=25)
-savefig('fig-Larmor.pdf')
+plt.savefig('fig-Larmor.pdf')
