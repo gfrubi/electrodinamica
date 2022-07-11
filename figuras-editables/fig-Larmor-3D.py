7a3507e1ed64e2f9a52b6136119174a633af94db
gfrubi <gfrubi@gmail.com>
2022-07-11 11:18:24 -0400

    Numpy now with module name

--- figuras-editables/fig-Larmor-3D.py
+++ figuras-editables/fig-Larmor-3D.py
@@ -1,24 +1,24 @@
-# coding: utf-8
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 plt.style.use('classic')
-from numpy import *
+import numpy as np
 
 fig = plt.figure()
 ax = Axes3D(fig)
 ax.set_axis_off() # desactiva ejes
 
-t = linspace(0, pi, 100)
-p = linspace(0, 2*pi-pi/2, 100)
-theta,phi = meshgrid(t,p)
+t = np.linspace(0, np.pi, 100)
+p = np.linspace(0, 2*np.pi-np.pi/2, 100)
+theta, phi = np.meshgrid(t,p)
 
-r = (sin(theta))**2
-x = r * sin(theta) * cos(phi)
-y = r * sin(theta) * sin(phi)
-z = r * cos(theta)
+r = (np.sin(theta))**2
+x = r * np.sin(theta) * np.cos(phi)
+y = r * np.sin(theta) * np.sin(phi)
+z = r * np.cos(theta)
 
 ax.plot_surface(x, y, z, rstride=4, cstride=4, color='red', alpha=0.5)
 #ax.view_init(elev=45, azim=0)
 ax.quiver(0,0,0,0,0,1, length=0.7, arrow_length_ratio=0.15, pivot='tail', color='blue', linewidth=5)
 ax.text(0.1,0,0.60, r'$\vec{a}$', fontsize=25)
-plt.savefig('fig-Larmor.pdf')
+#plt.savefig('fig-Larmor.pdf')
+plt.show()
