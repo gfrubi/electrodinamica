8cbbcb0e0e8980acc9876a0ef344c59cf8b55d69
Guillermo Rubilar <gfrubi@gmail.com>
2025-11-25 17:21:00 -0300

    nuevas figuras scattering the Thomson

---
+++ figuras-editables/fig-Thomson-02.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+plt.style.use('bmh')
+
+theta = np.linspace(0, 2*np.pi, 200)
+plt.subplot(1, 1, 1, projection='polar') #define el subplot de la figura en proyeccion polar
+r1 = 0.5*(1+np.cos(theta)**2)
+def r2(x):
+    return r1/(1+x*(1-np.cos(theta)))**2
+def r3(x):
+    return 0.5*(1+np.cos(theta)**2+(x**2*(1-np.cos(theta))**2)/(1+x*(1-np.cos(theta))))/(1+x*(1-np.cos(theta)))**2
+plt.plot(theta, r1, color='red', linewidth=2.0, label='Thomson')
+plt.plot(theta, r2(0.2), color='blue', linewidth=2.0, linestyle='--', label='Thomson+Compton, $x=0.2$')
+plt.plot(theta, r2(0.5), color='blue', linewidth=2.0, label='Thomson+Compton, $x=0.5$')
+plt.plot(theta, r3(0.2), color='green', linewidth=2.0, linestyle='--', label='Klein-Nishina, $x=0.2$')
+plt.plot(theta, r3(0.5), color='green', linewidth=2.0, label='Klein-Nishina, $x=0.5$')
+plt.text(0.07,1.12, r'$\theta$', fontsize=25)
+plt.legend(fontsize=7)
+plt.yticks([0.2,0.4,0.6,0.8,1],['','','','',''])
+plt.savefig("fig-Thomson-02.pdf")
+plt.show()
+
--- figuras-editables/fig-Thomson.py
+++ figuras-editables/fig-Thomson.py
@@ -3,15 +3,12 @@ import numpy as np
 plt.style.use('bmh')
 
 theta = np.linspace(0, 2*np.pi, 200)
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1, projection='polar') #define el subplot de la figura en proyeccion polar
-r = 0.5*(1+np.cos(theta)**2)
-ax.plot(theta, r, color='red', linewidth=2.0)
-ax.text(0.07,1.12, r'$\theta$', fontsize=25)
-
-
-# borra labels del eje radial
-for tick in ax.axes.get_yticklabels():
-        tick.set_visible(False)
+plt.subplot(1, 1, 1, projection='polar') #define el subplot de la figura en proyeccion polar
+r1 = 0.5*(1+np.cos(theta)**2)
+plt.plot(theta, r1, color='red', linewidth=2.0)
+plt.text(0.07,1.12, r'$\theta$', fontsize=25)
+plt.legend(fontsize=7)
+plt.yticks([0.2,0.4,0.6,0.8,1],['','','','',''])
 plt.savefig("fig-Thomson.pdf")
+plt.show()
 
