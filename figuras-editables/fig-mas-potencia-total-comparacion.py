851f30aca7d0193b14a7f4fbd4325440ca316ae8
Guillermo Rubilar <gfrubi@gmail.com>
2025-11-26 19:01:18 -0300

    actualizado código

--- figuras-editables/fig-mas-potencia-total-comparacion.py
+++ figuras-editables/fig-mas-potencia-total-comparacion.py
@@ -1,33 +1,29 @@
-# -*- coding: utf-8 -*-
-
-from numpy import *
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy import integrate
 from scipy.special import jv
-from matplotlib.pyplot import *
+plt.style.use('bmh')
 
-m = array(range(1,6))
-graficos = 2
+m = np.arange(1,6)
 betas = [0.5,0.1]
 colores = ['ro','bs']
-figure()
-for i in range(graficos):
-    b=betas[i]
+plt.figure()
+for b,c in zip(betas,colores):
 # se define el resultado de la integral, para cada m como una función
     def f(m):
-            f=lambda theta: (b**2)*m**2*(tan(theta)**2)*jv(m,b*m*cos(theta))**2*sin(theta)
-            # calcula la integral, usando el método de cuadraturas
-            return integrate.quad(f, 0, pi)[0]
+            f = lambda theta: (b**2)*m**2*(np.tan(theta)**2)*jv(m,b*m*np.cos(theta))**2*np.sin(theta)
+            return integrate.quad(f, 0, np.pi)[0] # calcula la integral, usando el método de cuadraturas
     # calcula la integral para toda la lista de valores de m
-    y = array(map(f,m))
-    # normaliza la integral, dividiendo por la suma sobre todos los m's
-    ynorm = y/sum(y)
+    f_vec = np.vectorize(f) # versión vectorizada
+    y = f_vec(m)
+    ynorm = y/np.sum(y) # normaliza la integral, dividiendo por la suma sobre todos los m's
     #grafica 
-    plot(m,ynorm,colores[i],label='$\\beta= $'+str(betas[i])) 
-legend()
-xlim(0.8,5.2)
-ylim(0,1.2)
-grid(True)
-minorticks_on()
-xlabel('$m$', fontsize=15)
-ylabel('$\left\langle {P_m}\\right\\rangle $',  fontsize=15)
-savefig("fig-mas-potencia-total-comparacion.pdf")
+    plt.plot(m,ynorm,c,label='$\\beta= $'+str(b), markersize=4) 
+plt.legend()
+plt.xlim(0.8,5.2)
+plt.ylim(0,1.2)
+plt.grid(True)
+plt.minorticks_on()
+plt.xlabel('$m$', fontsize=15)
+plt.ylabel('$\\left\\langle {P_m}\\right\\rangle /\\left\\langle {P}\,\\right\\rangle $',  fontsize=15)
+plt.savefig("fig-mas-potencia-total-comparacion.pdf")
